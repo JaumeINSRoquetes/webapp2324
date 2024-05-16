89464fe0ca9c12f3f02cf81c26a7251ec8c90e8d
Jaume Ferre <168031749+JaumeINSRoquetes@users.noreply.github.com>
2024-05-16 23:57:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function canvia_seccio(num_boto) {
 }
 let validat = false;    // variable que permet saber si hi ha algun usuari validat
 let nom, contrasenya;
-let scriptURL = "https://script.google.com/.../exec"    // s'ha de substituir la cadena de text per la URL del script
+let scriptURL = "https://script.google.com/macros/s/AKfycbwUn7-hIc7TXmfP8vCOQxIOex2xEAgcyTiZnabFQ3XG0rd8x5VJKynM9EBUmlUvpdpm/exec"    // s'ha de substituir la cadena de text per la URL del script
 
 function inici_sessio() {
     nom = document.getElementById("nom_usuari").value;    // la propietat "value" d'un quadre de text correspon al text escrit per l'usuari
