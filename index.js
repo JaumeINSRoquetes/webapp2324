8982bd0508b3f74d656d9c027081f685e80a8b32
Jaume Ferre <168031749+JaumeINSRoquetes@users.noreply.github.com>
2024-05-16 23:56:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function canvia_seccio(num_boto) {
             boto.style.color = "#950E17";    // es destaca la secció activa amb el canvi de colors del botó corresponent
             boto.style.backgroundColor = "#FCDEE0";
             seccio.style.display = "flex";    // es fa visible la secció activa
-            getElementById("seccio_0").style.display = "none";
         }
         else {
             boto.style.color = "white";    // colors dels botons de seccions inactives
