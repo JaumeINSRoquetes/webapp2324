cf34658b2295b8c1fb3241d71b364f032127e960
Jaume Ferre <168031749+JaumeINSRoquetes@users.noreply.github.com>
2024-05-17 12:18:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -71,11 +71,5 @@ function nou_usuari() {
         });
 }
 function tanca_sessio() {
-    if (validat) {
-        if (confirm("Vols tancar la sessió?")) {    // S'ha respost "Sí"
-            storage.setItem("usuari", "");
-            
-        }
-    }
-    location.reload();    // recàrrega de la pàgina, es reinicialitzen totes les variables
+        location.reload();    // recàrrega de la pàgina, es reinicialitzen totes les variables
 }
