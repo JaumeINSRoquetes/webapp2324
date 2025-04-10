924c6b88d69ea411cd7c3ca9c020179e340812f0
Jaume Ferre <168031749+JaumeINSRoquetes@users.noreply.github.com>
2025-04-10 09:50:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let validat = false;    // variable que permet saber si hi ha algun usuari validat
 let nom, contrasenya;
 let scriptURL = "https://script.google.com/macros/s/AKfycbwUn7-hIc7TXmfP8vCOQxIOex2xEAgcyTiZnabFQ3XG0rd8x5VJKynM9EBUmlUvpdpm/exec"    // s'ha de substituir la cadena de text per la URL del script
+
 function canvia_seccio(num_boto) {
     const menu = document.getElementById("menu");
     const num_botons = menu.children.length;    // el nombre de botons dins de l'element "menu"
